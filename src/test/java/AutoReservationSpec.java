f16ca8bc05bb760615a180e00ca7ef7e5a7d2491
Aya Ebata <ayaaaaa.0u0w@gmail.com>
2020-06-13 21:57:15 +0900

    selenideを追加

---
+++ src/test/java/AutoReservationSpec.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class AutoReservationSpec {
+
+    @Test
+    public void test() {
+        open("https://www.google.co.jp/");
+    }
+}
